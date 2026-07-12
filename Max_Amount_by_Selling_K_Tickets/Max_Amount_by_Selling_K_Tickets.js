43052120a1ec81864c66fea6682afec7462a8c60
Divyanshu Kumar Jha <divyanshukumarjha47@gmail.com>
2026-07-12 08:44:40 +0530

    Sync Max Amount by Selling K Tickets solution

---
+++ Max_Amount_by_Selling_K_Tickets/Max_Amount_by_Selling_K_Tickets.js
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxAmount(vector<int>& arr, int k) {
+        // code here
+        const int mod =1e9+7;
+        priority_queue<int> pq;
+        for(int x:arr){
+            pq.push(x);
+        }
+        long long ans=0;
+        while(k-- && !pq.empty()){
+            int x= pq.top();
+            pq.pop();
+            ans =(ans+x)%mod;
+            if(x>1){
+                pq.push(x-1);
+            }
+        }
+        return ans;
+    }
+};
